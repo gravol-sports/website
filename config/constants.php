2f476b4f7dd378af445fa56eb462343e50ddb007
Fahad Kamraan <fahadkamraan@gmail.com>
2019-05-01 16:01:49 -0700

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,10 +1,10 @@
 <?php
 
-define('EMAIL', 'fahad_kamraan@srmap.edu.in');
-define('PASSWORD', 'oyesfkoye');
-define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASS', 'kammy1');
+define('EMAIL', '');
+define('PASSWORD', '');
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 define('DB_NAME', 'gravol');
 
 ?>
